f146a59c8d03097786e387ee24d6ed230934c259
LuisFerUIO <fsuarez.an@gmail.com>
2025-08-20 23:38:45 -0500

    js animateback

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@ import.meta.glob([
 
 import domReady from '@roots/sage/client/dom-ready';
 import { animateText } from './animateText';
+import { animateBack } from "./animateBackground";
 
 domReady(() => {
   const animation = animateText();
