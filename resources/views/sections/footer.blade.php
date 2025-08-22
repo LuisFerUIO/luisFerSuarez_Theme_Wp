d5841669799631a415beecd4d80cf83d0bf1a7f5
LuisFerUIO <fsuarez.an@gmail.com>
2025-08-21 23:53:49 -0500

    footer

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="content-info bg-gray-900 text-white py-10">
+<footer class="content-info text-white py-10">
   <div class="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
     @for ($i = 1; $i <= 4; $i++)
       @if (is_active_sidebar('sidebar-footer-' . $i))
