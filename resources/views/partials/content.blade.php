3454ccc130ca3a9d993121d06b45edff12f172a8
LuisFerUIO <fsuarez.an@gmail.com>
2025-08-21 23:23:11 -0500

    tailwind

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -9,7 +9,13 @@
     @include('partials.entry-meta')
   </header>
 
-  <div class="border-t border-l border-red-500 rounded-tl-md p-4 entry-summary">
+  <div class="relative p-6 rounded-tl-lg bg-white">
+    <!-- Línea superior -->
+    <div class="absolute top-0 left-0 w-full h-1 bg-gradient-to-r from-red-500 to-orange-500 rounded-tl-lg"></div>
+    <!-- Línea izquierda -->
+    <div class="absolute top-0 left-0 h-full w-1 bg-gradient-to-b from-red-500 to-orange-500 rounded-tl-lg"></div>
+
     @php(the_excerpt())
+
   </div>
 </article>
